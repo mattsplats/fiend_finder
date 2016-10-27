e076ab2ecfdde95204f02d0f9e1d3d9970f36f93
Matt Davis <matt_davis@me.com>
2016-10-26 19:48:17 -0500

    Working app

---
+++ app/routing/html-routes.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+exports.home = function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public/home.html'));
+};
+
+exports.survey = function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public/survey.html'));
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+// Express vars
+const express    = require('express'),
+      bodyParser = require('body-parser'),
+      //path       = require('path'),
+
+      htmlRoutes = require('./app/routing/html-routes.js'),
+      apiRoutes  = require('./app/routing/api-routes.js'),
+
+      app        = express(),
+      PORT       = 3000;
+
+// Express init
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// HTML routes
+app.get('/', htmlRoutes.home);
+app.get('/survey', htmlRoutes.survey);
+
+// API routes
+app.get('/api/friends', apiRoutes.get);
+app.post('/api/friends', apiRoutes.post);
+
+// Init server
+app.listen(PORT, function () {
+	console.log('App listening on port ' + PORT);
+});
